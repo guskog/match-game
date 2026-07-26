6947f9b889923306221104b6ec925366c9bd79ec
gustav <61350395+guskog@users.noreply.github.com>
2026-07-27 01:46:53 +0200

    Added new music: Attribution Code "Vibing Over Venus" Kevin MacLeod (incompetech.com) Licensed under Creative Commons: By Attribution 4.0 License http://creativecommons.org/licenses/by/4.0/

--- main.gd
+++ main.gd
@@ -1,3 +1,8 @@
+# Music attribution
+#Attribution Code
+#"Vibing Over Venus" Kevin MacLeod (incompetech.com)
+#Licensed under Creative Commons: By Attribution 4.0 License
+#http://creativecommons.org/licenses/by/4.0/ 
 extends Node
 
 @export var mob_scene: PackedScene
